fb626a1b26a1bbd92a6c02d49e0a38dcab5224e9
jiyj <173654529@qq.com>
2021-09-09 14:27:51 +0800

    modify by kisang init

---
+++ src/main/java/com/lyc/hik/HikApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.lyc.hik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyc/hik/HikApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyc.hik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
